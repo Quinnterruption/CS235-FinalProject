082eec9d6351b240a061d5a908f579acc5313c63
Quinnterruption <83046743+Quinnterruption@users.noreply.github.com>
2026-04-13 23:54:25 -0500

    Removed old unnecessary code

--- extra/windowBuffer.h
+++ extra/windowBuffer.h
@@ -71,30 +71,6 @@ struct WindowBuffer {
     void drawTriangle(const vec3& a, const vec3& b, const vec3& c);
 
     void drawWireframe(const WireFrame& wireframe);
-
-    //    void drawLine(int x1, int y1, int x2, int y2) {
-//        int dx = abs(x2 - x1);
-//        int sx = x1 < x2 ? 1 : -1;
-//        int dy = -abs(y2 - y1);
-//        int sy = y1 < y2 ? 1 : -1;
-//        int err = dx + dy;
-//        int e2;
-//
-//        while (true) {
-//            drawAtSafe(x1, y1, 0, 255, 0);
-//
-//            if (x1 == x2 && y1 == y2) break;
-//            e2 = 2 * err;
-//            if (e2 >= dy) {
-//                err += dy;
-//                x1 += sx;
-//            }
-//            if (e2 <= dx) {
-//                err += dx;
-//                y1 += sy;
-//            }
-//        }
-//    }
 };
 
 /**
